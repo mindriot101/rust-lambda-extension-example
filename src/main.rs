4d0c4ffcdce24fa8ad8438bc7235e53a6a90f632
Simon Walker <s.r.walker101@googlemail.com>
2021-12-24 00:35:35 +0000

    Use trait for allowing state
    This new example tracks the number of invocations per runtime lifetime.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use lambda_extension::{extension_fn, Error, NextEvent};
+use lambda_extension::{Error, NextEvent, Extension};
+use std::{pin::Pin, future::{ready, Future}};
 
-async fn log_extension(event: NextEvent) -> Result<(), Error> {
-    match event {
-        NextEvent::Shutdown(event) => {
-            println!("Shutdown {:?}", event);
+#[derive(Default)]
+struct MyExtension {
+    num_events: usize,
+}
+
+impl Extension for MyExtension {
+    type Fut = Pin<Box<dyn Future<Output = Result<(), Error>>>>;
+
+    fn call(&mut self, event: NextEvent) -> Self::Fut {
+        match event {
+            NextEvent::Shutdown(_event) => {
+                println!("Shutdown handled {} events", self.num_events);
+            }
+            NextEvent::Invoke(event) => {
+                self.num_events += 1;
+                println!("Invoke {:?}", event);
+            }
         }
-        NextEvent::Invoke(event) => {
-            println!("Invoke {:?}", event);
-        }
+        Box::pin(ready(Ok(())))
     }
-    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = extension_fn(log_extension);
-    lambda_extension::run(func).await
+    lambda_extension::run(MyExtension::default()).await
 }
